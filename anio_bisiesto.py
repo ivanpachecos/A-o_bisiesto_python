ea8b5202adbeac0d5f35da156d59921d4b94916a
ivanpachecos <111659681+ivanpachecos@users.noreply.github.com>
2022-11-24 13:55:04 -0500

    Update anio_bisiesto.py

--- anio_bisiesto.py
+++ anio_bisiesto.py
@@ -1,10 +1,12 @@
-#Programa para ver si el año que ingreso es bisiesto o no
+#Crear una función para ver si el año que ingreso es bisiesto o no
 
 anio = int(input("Ingrese su nunero: "))
 
-if (anio % 4 == 0 and anio % 100 !=0) or anio % 400 ==0:
-  print(f"El año {anio}, es bisiesto")
+def anio_bisiesto():
+    if (anio % 4 == 0 and anio % 100 !=0) or anio % 400 ==0:
+        print(f"El año {anio}, es bisiesto")
 
-else:
-  print("No es bisiesto")
+    else:
+        print("No es bisiesto")
 
+anio_bisiesto()
